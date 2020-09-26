70250475704b49b3791feb904a3a7d80bf3ad1dd
ElK <62822011+evkonradi@users.noreply.github.com>
2020-09-26 10:51:32 -0500

    offline functionality is completed

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,6 @@
 let transactions = [];
 let myChart;
 
-fetch("/api/transaction")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    // save db data on global variable
-    transactions = data;
-
-    populateTotal();
-    populateTable();
-    populateChart();
-  });
-
 function populateTotal() {
   // reduce transaction amounts to a single total value
   let total = transactions.reduce((total, t) => {
